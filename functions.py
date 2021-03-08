39128b148e541accd21e33fcd1200c075ab662fe
Alexander Katrompas <alexander.katrompas@austincc.edu>
2021-03-08 16:15:08 -0600

    initial commit

---
+++ functions.py
@@ -0,0 +1,58 @@
+#
+#
+# your comment header here
+#
+#
+
+# SIZE is a global constant, use this throughout your program.
+# you should be able to change this to any integer > 0 and your
+# whole program will still work correctly. TEST THIS!
+SIZE = 10
+
+import numpy as np
+import random
+
+def make_array():
+    # create a numpy array of size SIZE filled with 0s.
+    # fill it random numbers 10-100 (inclusive) USING A LOOP
+    # return it
+    pass
+
+def total(nums):
+    # this will accept a Numpy integer array
+    # find the total of the elements of the array USING A LOOP
+    # you may not use the Numpy sum function or anything similar
+    # return the sum as an integer
+    pass
+
+def average(nums):
+    # this will accept a Numpy integer array
+    # find the average if the array by calling total() and dividing by SIZE
+    # you may not use the Numpy average function or anything similar
+    # return the average as a float which has ensures pure float
+    # calculations and guarantees maximum precision
+    pass
+
+def minimum(nums):
+    # this will accept a Numpy integer array
+    # find the minimum of the array USING A LOOP and return it
+    # you may not use the Numpy min function or anything similar
+    # return the minimum as an integer
+    pass
+
+def maximum(nums):
+    # this will accept a Numpy integer array
+    # find the maximum of the array USING A LOOP and return it
+    # you may not use the Numpy max function or anything similar
+    # return the maximum as an integer
+    pass
+
+def reverse_array(arr):
+    # this will accept a Numpy integer array
+    # do NOT alter the array passed in. use it as-is
+    # put the numbers from arr in another array in reverse order
+    # you MUST use a loop to do this
+    # return the reversed array
+    rev_nums = np.array([0] * SIZE, dtype=int)
+    pass
+
---
+++ main.py
@@ -0,0 +1,45 @@
+# 
+# 
+# your comment header here
+# 
+# 
+
+# import libraries you will need
+import functions as fn
+
+def main():
+    # this function is complete, DO NOT MODIFY IT
+    # you may modify it while you program, but when you turn in for grading
+    # it must be returned to EXACTLY this state
+    
+    # this will create and initialize the array with
+    # random numbers 10-100 (inclusive)
+    numbers = fn.make_array()
+    
+    # show the array
+    print("All the numbers...")
+    print(numbers)
+    print()
+    
+    print("The minimum is:", fn.minimum(numbers))
+    print("The maximum is:", fn.maximum(numbers))
+    print("The total is:", fn.total(numbers))
+    print("The average is:", fn.average(numbers))
+    print()
+
+    # this will reverse the array
+    numbers = fn.reverse_array(numbers)
+
+    # show the array
+    print("All the numbers...")
+    print(numbers)
+    print()
+    
+    print("The minimum is:", fn.minimum(numbers))
+    print("The maximum is:", fn.maximum(numbers))
+    print("The total is:", fn.total(numbers))
+    print("The average is:", fn.average(numbers))
+
+# this starts the program
+main()
+
