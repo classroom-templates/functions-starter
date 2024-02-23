64018f6d4dba4eef6f8a0dd8fc487458b05d8b22
Alexander Katrompas <alexander.katrompas@austincc.edu>
2024-02-23 17:04:19 -0600

    added comments

--- functions.py
+++ functions.py
@@ -8,12 +8,14 @@ import numpy as np
 import random as rnd
 
 def make_array(size, minr, maxr):
+    # put the function comment block here
     # create a numpy array of ints of length size
     # fill it random numbers minr to maxr (inclusive) USING A LOOP
     # return it
     pass
 
 def total(nums):
+    # put the function comment block here
     # this will accept a Numpy integer array
     # find the total of the elements of the array USING A LOOP
     # you may not use the Numpy sum function or anything similar
@@ -22,6 +24,7 @@ def total(nums):
     pass
 
 def average(nums):
+    # put the function comment block here
     # this will accept a Numpy integer array
     # find the average of the array by calling total() and dividing by num's length
     # you may not use the Numpy average function or anything similar
@@ -31,6 +34,7 @@ def average(nums):
     pass
 
 def minimum(nums):
+    # put the function comment block here
     # this will accept a Numpy integer array
     # find the minimum of the array USING A LOOP and return it
     # you may not use the Numpy min function or anything similar
@@ -39,6 +43,7 @@ def minimum(nums):
     pass
 
 def maximum(nums):
+    # put the function comment block here
     # this will accept a Numpy integer array
     # find the maximum of the array USING A LOOP and return it
     # you may not use the Numpy max function or anything similar
@@ -47,6 +52,7 @@ def maximum(nums):
     pass
 
 def reverse_array(arr):
+    # put the function comment block here
     # this will accept a Numpy integer array
     # do NOT alter the array passed in. use it as-is
     # put the numbers from arr in ANOTHER array in reverse order
@@ -55,4 +61,3 @@ def reverse_array(arr):
     # you may use the len() function to get the array's size prior to processing
     # return the reversed array
     pass
-
