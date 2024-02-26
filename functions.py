20b290c2733cba51328f5864f16ae7808ccfdc23
Alexander Katrompas <alexander.katrompas@austincc.edu>
2024-02-26 08:20:37 -0600

    added more instructional comments

--- functions.py
+++ functions.py
@@ -9,50 +9,77 @@ import random as rnd
 
 def make_array(size, minr, maxr):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+    
+    # instructions...
     # create a numpy array of ints of length size
     # fill it random numbers minr to maxr (inclusive) USING A LOOP
     # return it
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
 
 def total(nums):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+    
+    # instructions...
     # this will accept a Numpy integer array
     # find the total of the elements of the array USING A LOOP
     # you may not use the Numpy sum function or anything similar
     # you may use the len() function to get the array's size prior to processing
     # return the sum as an integer
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
 
 def average(nums):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+    
+    # instructions...
     # this will accept a Numpy integer array
     # find the average of the array by calling total() and dividing by num's length
     # you may not use the Numpy average function or anything similar
     # you may use the len() function to get the array's size prior to processing
     # return the average as a float which GUARNATEES pure float
     # calculations and guarantees maximum precision
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
 
 def minimum(nums):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+        
+    # instructions...
     # this will accept a Numpy integer array
     # find the minimum of the array USING A LOOP and return it
     # you may not use the Numpy min function or anything similar
     # you may use the len() function to get the array's size prior to processing
     # return the minimum as an integer
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
 
 def maximum(nums):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+    
     # this will accept a Numpy integer array
     # find the maximum of the array USING A LOOP and return it
     # you may not use the Numpy max function or anything similar
     # you may use the len() function to get the array's size prior to processing
     # return the maximum as an integer
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
 
 def reverse_array(arr):
     # put the function comment block here
+    # https://katrompas.accprofessors.com/commenting
+    
+    # instructions...
     # this will accept a Numpy integer array
     # do NOT alter the array passed in. use it as-is
     # put the numbers from arr in ANOTHER array in reverse order
@@ -60,4 +87,6 @@ def reverse_array(arr):
     # automated function or method that does this for you
     # you may use the len() function to get the array's size prior to processing
     # return the reversed array
+
+    # DELETE ALL THE INSTRUCTIONAL COMMENTS BEFORE SUBMISSION
     pass
